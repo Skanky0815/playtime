eb0e95572c30356e49d2ab944068b48b689bb5c4
Skanky0815 <rico-schulz@web.de>
2021-12-16 22:37:08 +0100

    remove unused

--- user/src/main/kotlin/org/playtime/user/UserService.kt
+++ user/src/main/kotlin/org/playtime/user/UserService.kt
@@ -3,7 +3,6 @@ package org.playtime.user
 import org.playtime.user.registration.Registration
 import org.playtime.user.registration.RegistrationData
 import org.playtime.user.registration.VerifyData
-import org.playtime.user.user.User
 import org.playtime.user.user.Users
 
 class UserService(
